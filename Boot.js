415cd66e31aac44eeb68755731413b1c7a0a7990
mahmoudswaid <mahmoudswaid@gmail.com>
2018-11-19 14:42:20 +0100

    Update Boot.js

--- Boot.js
+++ Boot.js
@@ -7,6 +7,13 @@ Game.Boot = function (game){
 
 Game.Boot.prototype = {
   init:function(){
-     
-  }
+     this.input.maxPointers = 1;
+     this.stage.disableVisibilityChage = true;
+  },
+   preload:function(){
+      this.load.image('preloaderBar', 'assets/preloader.png');
+   },
+   create:function(){
+      this.state.start('Proloader');
+   }
 }
